985879482e2bb5eec96d786dda79936feba04b3b
Justin Chiu <justin.chiu@stonybrook.edu>
2017-05-19 14:13:38 -0400

    v2.0

---
+++ js/onView.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+ 
+  $('#onView').css('opacity', '0');
+ 
+  $('#onView').waypoint(function() {
+      $('#onView').addClass('bounceInLeft');
+      $('#onView').css('opacity', '100');
+  }, { offset: '80%' });
+ 
+});
+
+$(document).ready(function(){
+ 
+  $('#course_ani').css('opacity', '0');
+ 
+  $('#course_ani').waypoint(function() {
+      $('#course_ani').addClass('bounceInLeft');
+      $('#course_ani').css('opacity', '100');
+  }, { offset: '80%' });
+ 
+});
+
+$(document).ready(function(){
+ 
+  $('#exp_ani').css('opacity', '0');
+ 
+  $('#exp_ani').waypoint(function() {
+      $('#exp_ani').addClass('bounceInLeft');
+      $('#exp_ani').css('opacity', '100');
+  }, { offset: '80%' });
+ 
+});
+
+$(document).ready(function(){
+ 
+  $('#proj_ani').css('opacity', '0');
+ 
+  $('#proj_ani').waypoint(function() {
+      $('#proj_ani').addClass('bounceInLeft');
+      $('#proj_ani').css('opacity', '100');
+  }, { offset: '80%' });
+ 
+});
+
+$(document).ready(function(){
+ 
+  $('#res_ani').css('opacity', '0');
+ 
+  $('#res_ani').waypoint(function() {
+      $('#res_ani').addClass('bounceInLeft');
+      $('#res_ani').css('opacity', '100');
+  }, { offset: '80%' });
+ 
+});
+
+$(document).ready(function(){
+ 
+  $('#contact_ani').css('opacity', '0');
+ 
+  $('#contact_ani').waypoint(function() {
+      $('#contact_ani').addClass('bounceInLeft');
+      $('#contact_ani').css('opacity', '100');
+  }, { offset: '80%' });
+ 
+});
---
+++ js/typing_intro.js
@@ -0,0 +1,21 @@
+
+$(function(){
+		$(".type_intro").typed({
+			strings: ["Hi there!^1000<br>Welcome to my website.^1000", "I'm "],
+        showCursor: false,
+        typeSpeed: 0.6,
+        backSpeed: 0.6,
+      callback: function(){
+        part2();
+      },
+		});
+    function part2(){
+      $(".type_intro2").typed({
+          strings: ["a sophomore at Stony Brook University.^500", "a Computer Science major.^500", "a designer.^500", "a developer.^500", "a photographer.^500", "an engineer.^500", "Justin Chiu."],
+          showCursor: false,
+          typeSpeed: 0.8,
+            backSpeed: 0.6,
+      });
+
+    }
+});
