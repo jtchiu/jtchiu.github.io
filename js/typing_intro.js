0716992b5f732ccaed50ff6c62e9783e086c24bd
Justin Chiu <justin.chiu@stonybrook.edu>
2017-09-06 20:26:31 -0400

    changed typing intro to be shorter and be junior at stony brook

--- js/typing_intro.js
+++ js/typing_intro.js
@@ -11,7 +11,7 @@ $(function(){
 		});
     function part2(){
       $(".type_intro2").typed({
-          strings: ["a sophomore at Stony Brook University.^500", "a Computer Science major.^500", "a designer.^500", "a developer.^500", "a photographer.^500", "an engineer.^500", "Justin Chiu."],
+          strings: ["a junior at Stony Brook University.^500", "a Computer Science major.^500", "a designer.^500", "a developer.^500", "an engineer.^500", "Justin Chiu."],
           showCursor: false,
           typeSpeed: 0.8,
             backSpeed: 0.6,
