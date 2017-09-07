7b5a30fb66714ae07dd26750312810f2f9889082
Justin Chiu <justin.chiu@stonybrook.edu>
2017-09-06 20:18:56 -0400

    changed title animation to slideInLeft and made a couple project tweeks

--- js/onView.js
+++ js/onView.js
@@ -1,10 +1,10 @@
-// bounceInLeft
+// slideInLeft
 $(document).ready(function(){
  
   $('#onView').css('opacity', '0');
  
   $('#onView').waypoint(function() {
-      $('#onView').addClass('bounceInLeft');
+      $('#onView').addClass('slideInLeft');
       $('#onView').css('opacity', '100');
   }, { offset: '80%' });
  
@@ -15,7 +15,7 @@ $(document).ready(function(){
   $('#course_ani').css('opacity', '0');
  
   $('#course_ani').waypoint(function() {
-      $('#course_ani').addClass('bounceInLeft');
+      $('#course_ani').addClass('slideInLeft');
       $('#course_ani').css('opacity', '100');
   }, { offset: '80%' });
  
@@ -26,7 +26,7 @@ $(document).ready(function(){
   $('#exp_ani').css('opacity', '0');
  
   $('#exp_ani').waypoint(function() {
-      $('#exp_ani').addClass('bounceInLeft');
+      $('#exp_ani').addClass('slideInLeft');
       $('#exp_ani').css('opacity', '100');
   }, { offset: '80%' });
  
@@ -37,7 +37,7 @@ $(document).ready(function(){
   $('#work_exp_ani').css('opacity', '0');
  
   $('#work_exp_ani').waypoint(function() {
-      $('#work_exp_ani').addClass('bounceInLeft');
+      $('#work_exp_ani').addClass('slideInLeft');
       $('#work_exp_ani').css('opacity', '100');
   }, { offset: '80%' });
  
@@ -47,7 +47,7 @@ $(document).ready(function(){
  
   $('#proj_ani').css('opacity', '0');
   $('#proj_ani').waypoint(function() {
-    $('#proj_ani').addClass('bounceInLeft');
+    $('#proj_ani').addClass('slideInLeft');
     $('#proj_ani').css('opacity', '100');
   }, { offset: '80%' });
  
@@ -57,7 +57,7 @@ $(document).ready(function(){
  
   $('#res_ani').css('opacity', '0');
   $('#res_ani').waypoint(function() {
-    $('#res_ani').addClass('bounceInLeft');
+    $('#res_ani').addClass('slideInLeft');
     $('#res_ani').css('opacity', '100');
   }, { offset: '80%' });
  
@@ -67,7 +67,7 @@ $(document).ready(function(){
  
   $('#contact_ani').css('opacity', '0');
   $('#contact_ani').waypoint(function() {
-    $('#contact_ani').addClass('bounceInLeft');
+    $('#contact_ani').addClass('slideInLeft');
     $('#contact_ani').css('opacity', '100');
   }, { offset: '80%' });
  
